4e8cc5c36119af2d896f1f59fbf5938a40497c35
Dmytro Maior <mayor4090Gmail.com>
2023-10-15 20:08:06 +0300

    Change library for password hashing

--- src/auth/domain/users.py
+++ src/auth/domain/users.py
@@ -1,6 +1,6 @@
 from typing import TYPE_CHECKING, Any
 
-from passlib.hash import argon2
+import bcrypt
 
 from auth.domain.base import BaseModel
 
@@ -8,19 +8,27 @@ if TYPE_CHECKING:
     from auth.domain.events import Event
 
 
+def _hash_password(password: str) -> str:
+    return bcrypt.hashpw(password.encode(), bcrypt.gensalt()).decode()
+
+
+def _check_password(password: str, hashed_password: str) -> bool:
+    return bcrypt.checkpw(password.encode(), hashed_password.encode())
+
+
 class User(BaseModel):
     def __init__(self, id: str, username: str, email: str, password: str) -> None:
         super().__init__(id)
         self.username: str = username
         self.email: str = email
-        self._hashed_password: Any = argon2.hash(password)
+        self._hashed_password: Any = _hash_password(password)
         self.events: list[Event] = []
 
     def set_password(self, password: str) -> None:
-        self._hashed_password = argon2.hash(password)
+        self._hashed_password = _hash_password(password)
 
     def check_password(self, password: str) -> bool:
-        return argon2.verify(password, self._hashed_password)
+        return _check_password(password, self._hashed_password)
 
     def __eq__(self, other: object) -> bool:
         if not isinstance(other, User):
